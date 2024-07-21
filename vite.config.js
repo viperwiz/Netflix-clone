a2932365f94b6461a68f1e2023e3f659330d333a
rabin sedhai <bisheshsedhai12@gmail.com>
2024-07-21 20:56:44 +0545

    c

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', 
-    rollupOptions: {
-      input: {
-        main: '/src/main.jsx' 
-      }
-    }
+    outDir: 'dist' 
   }
 });
